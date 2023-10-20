1e24b95ca572f99a930409a57abf4b6c9620fcd8
Ednilson <ednilsoncastanheira53@gmail.com>
2023-10-20 19:22:06 +0100

    modified getAll to handle errors

--- controllers/nba_players.js
+++ controllers/nba_players.js
@@ -2,12 +2,19 @@ const { response } = require("express");
 const mongodb = require("../db/connect");
 const ObjectId = require("mongodb").ObjectId;
 
-const getAll = async (req, res, next) => {
-  const result = await mongodb.getDb().db().collection("nba_players").find();
-  result.toArray().then((lists) => {
-    res.setHeader("Content-Type", "application/json");
-    res.status(200).json(lists);
-  });
+const getAll = (req, res) => {
+  mongodb
+    .getDb()
+    .db()
+    .collection("nba_players")
+    .find()
+    .toArray((err, lists) => {
+      if (err) {
+        res.status(400).json({ message: err });
+      }
+      res.setHeader("Content-Type", "application/json");
+      res.status(200).json(lists);
+    });
 };
 
 const createPlayer = async (req, res) => {
