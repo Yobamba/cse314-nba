5441809dc75026204a85e71d3ff363aa442dcf12
Ednilson <ednilsoncastanheira53@gmail.com>
2023-10-20 19:24:45 +0100

    added validation and error handling to getSingle

--- controllers/nba_players.js
+++ controllers/nba_players.js
@@ -85,17 +85,23 @@ const deletePlayer = async (req, res) => {
   }
 };
 
-const getSingle = async (req, res, next) => {
+const getSingle = (req, res) => {
+  if (!ObjectId.isValid(req.params.id)) {
+    res.status(400).json("Must use a valid contact id to find a contact.");
+  }
   const userId = new ObjectId(req.params.id);
-  const result = await mongodb
+  mongodb
     .getDb()
     .db()
     .collection("nba_players")
-    .find({ _id: userId });
-  result.toArray().then((lists) => {
-    res.setHeader("Content-Type", "application/json");
-    res.status(200).json(lists[0]);
-  });
+    .find({ _id: userId })
+    .toArray((err, result) => {
+      if (err) {
+        res.status(400).json({ message: err });
+      }
+      res.setHeader("Content-Type", "application/json");
+      res.status(200).json(result[0]);
+    });
 };
 
 module.exports = {
